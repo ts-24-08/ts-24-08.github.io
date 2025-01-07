2b1b1f6ebac62112dd993682b0d66a5e27701270
Marco Lindner <marco.lindner@gmx.org>
2025-01-07 13:06:34 +0100

    Add dynamic Articles and styling

---
+++ blog/src/articles.js
@@ -0,0 +1,46 @@
+const articles = [
+  {
+    id: 1,
+    title: "How I improved my coding skills",
+    img: "dev-blog-1.jpg",
+    date: "2021-07-01",
+    author: "John Doe",
+    teaser: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    tags: ["coding", "programming", "learning"],
+  },
+  {
+    id: 2,
+    title: "Why I love JavaScript",
+    img: "dev-blog-2.jpg",
+    date: "2021-07-02",
+    author: "Jane Doe",
+    teaser: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    tags: ["coding", "programming", "javascript"],
+  },
+  {
+    id: 3,
+    title: "The benefits of remote work",
+    img: "dev-blog-3.jpg",
+    date: "2021-07-03",
+    author: "John Doe",
+    teaser: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    tags: ["work", "remote", "benefits"],
+  },
+];
+
+const authors = [
+  {
+    id: 1,
+    name: "John Doe",
+    img: "author-1.jpg",
+    bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+  },
+  {
+    id: 2,
+    name: "Jane Doe",
+    img: "author-2.jpg",
+    bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+  },
+];
+
+export default articles;
--- blog/src/script.js
+++ blog/src/script.js
@@ -1,1 +1,27 @@
 import "./input.css";
+import articles from "./articles.js";
+
+const section = document.querySelector("#recent-blog-posts");
+section.innerHTML = "";
+
+articles.forEach((article) => {
+  const articleElement = document.createElement("article");
+  articleElement.classList.add("mb-4", "border", "w-[391px]");
+  articleElement.innerHTML = `
+    <img src="${article.img}" alt="${article.title}" />
+    <p class="text-sm text-[#6941C6] font-semibold">${article.author.name} - ${
+    article.date
+  }</p>
+    <h1 class="text-xl font-bold my-4">${article.title}</h1>
+    <p>${article.teaser}</p>
+    <ul class="flex">
+      ${article.tags
+        .map(
+          (tag) =>
+            `<li><span class="bg-[#F9F5FF] text-[#6941C6] px-2 py-1 rounded-md mr-2">${tag}</span></li>`
+        )
+        .join("")}
+    </ul>
+    `;
+  section.appendChild(articleElement);
+});
